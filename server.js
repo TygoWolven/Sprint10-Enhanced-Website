415d9074d6042f4e1a1d9c2404a71b67288bb35d
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-04-26 11:34:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import express from 'express'
 // Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-const baseUrl = 'https://fdnd-agency.directus.app/'
-
 // Maak een nieuwe express app aan
 const app = express()
 
@@ -21,7 +19,10 @@ app.use(express.static('public'))
 // Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
 
-// Deze Array verzamelt berichten
+// Deze const zorgt voor een verkort gebruik van de Database URL
+const baseUrl = 'https://fdnd-agency.directus.app/'
+
+// Deze Array verzamelt likes
 const likes = [] 
 
 // GET Routes voor alle pagina's 
@@ -34,12 +35,42 @@ app.get('/', function(request, response) {
 	})
 })
 
+// Maak een POST route voor de index
+app.post('/', function (request, response) {
+	likes.push(request.body.like)
+	
+	response.redirect(303, '/')
+})
+
 app.get('/initiatief/:initiatief', function(request, response) {
 	fetchJson('https://fdnd-agency.directus.app/items/dh_services?filter={"id":' + request.params.initiatief + '}').then((HallenDataUitDeAPI) => {
 		response.render('initiatief', {
 			hallenData: HallenDataUitDeAPI.data[0]
 		})
 	})
+})
+
+// Als we vanuit de browser een POST doen op de detailpagina van een persoon
+app.post('/initiatief/:id', function (request, response) {
+	// Stap 1: Haal de huidige data op, zodat we altijd up-to-date zijn, en niks weggooien van anderen
+  
+	// Haal eerst de huidige gegevens voor dit board op, uit de WHOIS API
+	fetchJson(`${baseUrl}items/dh_services/${request.params.id}`).then(({ data }) => {
+	  // Stap 2: Sla de nieuwe data op in de API
+	  // Voeg de nieuwe lijst messages toe in de WHOIS API, via een PATCH request
+	  fetch(`${baseUrl}items/dh_services/${request.params.id}`, {
+		method: 'PATCH',
+		body: JSON.stringify({
+		  likes: data.likes + 1,
+		}),
+		headers: {
+		  'Content-type': 'application/json; charset=UTF-8',
+		},
+	  }).then((patchResponse) => {
+		// Redirect naar de persoon pagina
+		response.redirect(303, '/initiatief/' + request.params.id)
+	  })
+   })
 })
 
 app.get('/aanvraag', function(request, response) {
@@ -65,36 +96,6 @@ app.get('/faq', function(request, response) {
 		})
 	})
 })
-
-// Maak een POST route voor de index
-app.post('/', function (request, response) {
-	likes.push(request.body.like)
-	
-	response.redirect(303, '/')
-  })
-
-// Als we vanuit de browser een POST doen op de detailpagina van een persoon
-app.post('/initiatief/:id', function (request, response) {
-	// Stap 1: Haal de huidige data op, zodat we altijd up-to-date zijn, en niks weggooien van anderen
-  
-	// Haal eerst de huidige gegevens voor dit board op, uit de WHOIS API
-	fetchJson(`${baseUrl}items/dh_services/${request.params.id}`).then(({ data }) => {
-	  // Stap 2: Sla de nieuwe data op in de API
-	  // Voeg de nieuwe lijst messages toe in de WHOIS API, via een PATCH request
-	  fetch(`${baseUrl}items/dh_services/${request.params.id}`, {
-		method: 'PATCH',
-		body: JSON.stringify({
-		  likes: data.likes + 1,
-		}),
-		headers: {
-		  'Content-type': 'application/json; charset=UTF-8',
-		},
-	  }).then((patchResponse) => {
-		// Redirect naar de persoon pagina
-		response.redirect(303, '/initiatief/' + request.params.id)
-	  })
-	})
-  })
 
 // Poortnummer voor de LocalHost
 app.set('port', process.env.PORT || 8000)
