34c8e761072c1602eddb97983a5abd2fb9dfe29a
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-05-05 14:37:50 +0200

    .

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -10,18 +10,15 @@ function updateProgressBar() {
 
 // Functie voor de 'Initiatieven'
 const listVraag = document.getElementById('listVraag'),
-      listAanbod = document.getElementById('listAanbod'),
-      listError = document.getElementById('listError')
+      listAanbod = document.getElementById('listAanbod')
 
 function showList(val) {
     if(val==1) {
         listVraag.style.display='flex'
         listAanbod.style.display='none'
-        listError.style.display='none'
     } if(val==2) {
         listVraag.style.display='none'
         listAanbod.style.display='flex'
-        listError.style.display='none'
     }
 }
 
